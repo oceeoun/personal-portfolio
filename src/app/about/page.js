7fe554236c2369757946415d6c94c6e27c8c2de2
1004yeeun <joycejeoung@ucla.edu>
2025-12-26 22:08:01 -0800

    Updated about me page with project button

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,9 @@
+"use client";
 import NavBar from '../components/NavBar'
 import AboutSection from '../components/AboutSection'
-import ProjectSection from '../components/ProjectSection'
+import useClickSound from '../hooks/useClickSound'; 
 import Footer from '../components/Footer'
+import Link from 'next/link';
 import localFont from 'next/font/local'
 
 const satoshi = localFont({
@@ -11,6 +13,8 @@ const satoshi = localFont({
 });
 
 export default function AboutPage() {
+    const playClick = useClickSound();
+
     return (
         <main className="flex min-h-screen flex-col bg-[#DCD7D3]">
             <NavBar />
@@ -20,10 +24,15 @@ export default function AboutPage() {
                 </div>
                 <AboutSection />
             </div>
-            <div className="mx-auto px-7 pt-5 pb-15 sm:pt-10">
-                <ProjectSection />
+            <br/>
+            <div className="text-center px-5">
+            <Link href="/projects"><button onClick={() => {playClick();}} className="px-6 py-3 w-full sm:w-fit font-bold rounded-full mt-4 mr-4 bg-gradient-to-br from-gray-600 via-black to-gray-600 hover:bg-gradient-to-br hover:from-[#F0EDE9] hover:via-[#E6E1DD] hover:to-[#F0EDE9] text-white hover:text-black">view my projects →
+                    <span className="block hover:text-black text-white rounded-full"></span>
+                </button>
+            </Link>
             </div>
-
+            <br/>
+            <br/>
             <Footer />
         </main >
     );
